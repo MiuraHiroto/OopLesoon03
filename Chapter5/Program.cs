6f594f5aa869a59c81321dd1fe3c7f6264f5e193
MiuraHiroto <31021@ojs.ac.jp>
2020-10-02 15:54:55 +0900

    演習5.1、5.2

---
+++ Chapter5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5.1
+            Console.Write("文字列1");
+            var str1 = Console.ReadLine();
+            Console.Write("文字列2");
+            var str2 = Console.ReadLine();
+            if (String.Compare(str1, str2, true) == 0)
+                Console.WriteLine("等しい");
+            else
+                Console.WriteLine("等しくない");
+
+            //5.2
+            Console.Write("数値文字列:");
+            var line = Console.ReadLine();
+            int num;//変更後の数値格納用
+           
+            if (int.TryParse(line,out num))
+            {
+                Console.WriteLine(num.ToString("#,#"));
+            } else
+            {
+                Console.WriteLine("数値文字列ではありません");
+            }
+        }       
+    }
+}
