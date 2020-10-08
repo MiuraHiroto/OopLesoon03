26b2210286ed3b9bedcb020901b56b55a5a096f7
MiuraHiroto <31021@ojs.ac.jp>
2020-10-08 11:57:35 +0900

    2020/10/08

--- Capter6/Program.cs
+++ Capter6/Program.cs
@@ -12,67 +12,50 @@ namespace Capter6
     {
         static void Main(string[] args)
         {
-            /*var numbers = Enumerable.Range(1, 20).ToArray();
-            foreach (var item in numbers)
+            //整数の例
+            var numbers = new List<int> { 19, 17, 15, 24, 12, 25, 14, 20, 12, 28, 19, 30, 24, };
+
+            /*var strings = numbers.Distinct().Select(n => n.ToString("0000")).ToArray();
+            foreach (var str in strings)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(str + " ");
             }*/
 
-            var numbers = new List<int> { 9, 7, -5, 4, 2, 5, -4, 0, 4, -1, 0, 4, };
-            Console.WriteLine($"平均値:{numbers.Average()}");
-            Console.WriteLine($"合計値:{numbers.Sum()}");
-            Console.WriteLine($"最小値:{numbers.Where(n => n > 0 ).Min()}");
-            Console.WriteLine($"最大値:{numbers.Max()}");
-
-            bool exists = numbers.Any(n => n % 7 == 0);
-            
-            var results = numbers.Where(n => n > 0).Take(3);
-            foreach (var result in results)
+            numbers.Select(n => n.ToString("0000")).Distinct().ToList().ForEach(s => Console.Write(s + " "));
+            //Console.WriteLine(); //改行
+            //var sortedNumbers = numbers.OrderBy(n => n);
+            //並べ替え
+            Console.WriteLine(); //改行
+            var sortedNumbers = numbers.OrderBy(n => n);
+            foreach (var nums in sortedNumbers)
             {
-                Console.WriteLine(result + " ");
+                Console.Write(nums + " ");
             }
 
+            //文字列
+            var words = new List<string>
+            {
+                  "Microsoft","Apple","Google","Oracle","Facebook",
+            };
+            var lower = words.Select(name => name.ToLower().ToArray());
 
+            //オブジェクトの例
             var books = Books.GetBooks();
-            Console.WriteLine($"本の平均価格:{ books.Average(x => x.Price)}");
-            Console.WriteLine($"本の合計価格:{ books.Sum(x => x.Price)}");
-            Console.WriteLine($"本のページが最大:{ books.Max(x => x.Pages)}");
-            Console.WriteLine($"本の価格最大:{ books.Max(x => x.Price)}");
-            Console.WriteLine($"タイトルに「物語」がある冊数:{ books.Count(x => x.Title.Contains("物語"))}");
+            //タイトルリスト
+            var titles = books.Select(name => name.Title);
 
-            //600ページを超える書籍はあるかないか？(Any)
-            Console.WriteLine("\n600ページを超える書籍は");
-            Console.WriteLine(books.Any(n => n.Pages > 600) ? "ある" : "ない");
-            /*bool exit = books.Any(n => n.Pages >= 600);
-            if (exit)
+            foreach (var title in titles)
             {
-                Console.WriteLine("ある");
-            } else
-            {
-                Console.WriteLine("ない");
-            }*/
-            //すべてが200ページ以上の書籍か？(All)
-            Console.WriteLine("\nすべてが200ページを超える書籍は");
-            Console.WriteLine(books.All(x => x.Pages > 200) ? "200ページ以上" : "200ページ以下");
-            //400ページを超える本は何冊目か？(FirstOrDefaultメソッド)
-            //var book = books.FirstOrDefault(x => x.Pages > 400);
-            /*int i;
-            for ( i = 0; i < books.Count; i++)
-            {
-                if (books[i].Title.Contains(book.Title))
-                {
-                    break;
-                }
-            }*/
-            var count = books.FindIndex(x => x.Pages > 400);
-            Console.WriteLine($"400ページを超える本は{count + 1}冊目です。");
-
-            //本の値段が400円以上のものを3冊表示
-            var counts = books.Where(x => x.Price >= 400).Take(3);
-            foreach (var item in counts)
-            {
-                Console.WriteLine(item.Title + " ");
+                Console.Write(title + " ");
             }
+            //ページ数の多い順に並び替え(または金額の高い順)
+            var booke = books.OrderByDescending(book => book.Pages).Take(3);
+            foreach (var book in booke)
+            {
+                Console.WriteLine(book.Title + " " + book.Pages);
+            }
+            //var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35, };
+            
         }
     }
 }
