b8887037a15f1199c041f7ce987b2925578f51d3
MiuraHiroto <31021@ojs.ac.jp>
2020-10-08 10:59:20 +0900

    p117まで

--- Capter6/Program.cs
+++ Capter6/Program.cs
@@ -24,12 +24,55 @@ namespace Capter6
             Console.WriteLine($"最小値:{numbers.Where(n => n > 0 ).Min()}");
             Console.WriteLine($"最大値:{numbers.Max()}");
 
+            bool exists = numbers.Any(n => n % 7 == 0);
+            
+            var results = numbers.Where(n => n > 0).Take(3);
+            foreach (var result in results)
+            {
+                Console.WriteLine(result + " ");
+            }
+
+
             var books = Books.GetBooks();
             Console.WriteLine($"本の平均価格:{ books.Average(x => x.Price)}");
             Console.WriteLine($"本の合計価格:{ books.Sum(x => x.Price)}");
             Console.WriteLine($"本のページが最大:{ books.Max(x => x.Pages)}");
             Console.WriteLine($"本の価格最大:{ books.Max(x => x.Price)}");
             Console.WriteLine($"タイトルに「物語」がある冊数:{ books.Count(x => x.Title.Contains("物語"))}");
+
+            //600ページを超える書籍はあるかないか？(Any)
+            Console.WriteLine("\n600ページを超える書籍は");
+            Console.WriteLine(books.Any(n => n.Pages > 600) ? "ある" : "ない");
+            /*bool exit = books.Any(n => n.Pages >= 600);
+            if (exit)
+            {
+                Console.WriteLine("ある");
+            } else
+            {
+                Console.WriteLine("ない");
+            }*/
+            //すべてが200ページ以上の書籍か？(All)
+            Console.WriteLine("\nすべてが200ページを超える書籍は");
+            Console.WriteLine(books.All(x => x.Pages > 200) ? "200ページ以上" : "200ページ以下");
+            //400ページを超える本は何冊目か？(FirstOrDefaultメソッド)
+            //var book = books.FirstOrDefault(x => x.Pages > 400);
+            /*int i;
+            for ( i = 0; i < books.Count; i++)
+            {
+                if (books[i].Title.Contains(book.Title))
+                {
+                    break;
+                }
+            }*/
+            var count = books.FindIndex(x => x.Pages > 400);
+            Console.WriteLine($"400ページを超える本は{count + 1}冊目です。");
+
+            //本の値段が400円以上のものを3冊表示
+            var counts = books.Where(x => x.Price >= 400).Take(3);
+            foreach (var item in counts)
+            {
+                Console.WriteLine(item.Title + " ");
+            }
         }
     }
 }
