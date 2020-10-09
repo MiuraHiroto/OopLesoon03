d31bc18c278bc46a008e0d162438758899d58dcb
MiuraHiroto <31021@ojs.ac.jp>
2020-10-09 14:23:15 +0900

    6.1章

--- Capter6/Program.cs
+++ Capter6/Program.cs
@@ -12,50 +12,33 @@ namespace Capter6
     {
         static void Main(string[] args)
         {
-            //整数の例
-            var numbers = new List<int> { 19, 17, 15, 24, 12, 25, 14, 20, 12, 28, 19, 30, 24, };
-
-            /*var strings = numbers.Distinct().Select(n => n.ToString("0000")).ToArray();
-            foreach (var str in strings)
-            {
-                Console.WriteLine(str + " ");
-            }*/
-
-            numbers.Select(n => n.ToString("0000")).Distinct().ToList().ForEach(s => Console.Write(s + " "));
-            //Console.WriteLine(); //改行
-            //var sortedNumbers = numbers.OrderBy(n => n);
-            //並べ替え
-            Console.WriteLine(); //改行
-            var sortedNumbers = numbers.OrderBy(n => n);
-            foreach (var nums in sortedNumbers)
-            {
-                Console.Write(nums + " ");
-            }
-
-            //文字列
-            var words = new List<string>
-            {
-                  "Microsoft","Apple","Google","Oracle","Facebook",
-            };
-            var lower = words.Select(name => name.ToLower().ToArray());
-
-            //オブジェクトの例
-            var books = Books.GetBooks();
-            //タイトルリスト
-            var titles = books.Select(name => name.Title);
-
-            foreach (var title in titles)
-            {
-                Console.Write(title + " ");
-            }
-            //ページ数の多い順に並び替え(または金額の高い順)
-            var booke = books.OrderByDescending(book => book.Pages).Take(3);
-            foreach (var book in booke)
-            {
-                Console.WriteLine(book.Title + " " + book.Pages);
-            }
-            //var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35, };
             
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35, };
+
+            //6.1.1
+            Console.WriteLine(numbers.Max());
+            //6.1.2
+            /*var nums = numbers.Reverse().Take(2);
+            foreach (var num in nums)
+            {
+                Console.Write(num + " ");
+            }*/
+            int pos = numbers.Length - 2;
+            foreach (var number in numbers.Skip(pos))
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            //6.1.3
+            numbers.Select(x => x.ToString()).ToList().ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+            //6.1.4
+            foreach (var item in numbers.OrderBy(n => n).Take(3))
+            {
+                Console.Write(item + " ");
+            }
+            //6.1.5
+            Console.WriteLine(numbers.Distinct().Count(n => n > 10)); 
         }
     }
 }
