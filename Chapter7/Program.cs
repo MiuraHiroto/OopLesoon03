7fa1a80b050cee78f5051c83ecac6e6689c300f1
MiuraHiroto <31021@ojs.ac.jp>
2020-10-15 10:03:05 +0900

    p.119まで

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -78,11 +78,40 @@ namespace Chapter7
             var s = dict[md];
             Console.WriteLine(s);
             */
-            var lines = File.ReadAllLines("sample.txt");
+            /*var lines = File.ReadAllLines("sample.txt");
             var we = new WordsExtractor(lines);
             foreach (var word in we.Extract())
             {
                 Console.WriteLine(word);
+            }*/
+            DuplicateKeySample();
+        }
+        static public void DuplicateKeySample()
+        {
+            // ディクショナリの初期化
+            var dict = new Dictionary<string, List<string>>() {
+               { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
+               { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
+            };
+
+            // ディクショナリに追加
+            var key = "EC";
+            var value = "電子商取引";
+            if (dict.ContainsKey(key))
+            {
+                dict[key].Add(value);
+            } else
+            {
+                dict[key] = new List<string> { value };
+            }
+
+            // ディクショナリの内容を列挙
+            foreach (var item in dict)
+            {
+                foreach (var term in item.Value)
+                {
+                    Console.WriteLine("{0} : {1}", item.Key, term);
+                }
             }
         }
     }
