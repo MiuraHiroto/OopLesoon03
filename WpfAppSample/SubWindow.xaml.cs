20d218f2d7c37eaf77b4664f09fd9b81164e64f5
MiuraHiroto <31021@ojs.ac.jp>
2020-10-30 15:33:41 +0900

    SendMailApp新規作成

---
+++ WpfAppSample/SubWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppSample
+{
+    /// <summary>
+    /// SubWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SubWindow : Window
+    {
+        public SubWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            checkBoxTextBlock.Text = "チェック済み";
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            checkBoxTextBlock.Text = "未チェック";
+        }
+
+        private void redRadioButten_Checked(object sender, RoutedEventArgs e)
+        {
+            colorTextBox.Text = "赤";
+        }
+
+        private void yellowRadioButten_Checked(object sender, RoutedEventArgs e)
+        {
+            colorTextBox.Text = "黄";
+        }
+
+        private void blueRadioButten_Checked(object sender, RoutedEventArgs e)
+        {
+            colorTextBox.Text = "青";
+        }
+
+        private void seasonConboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            seasonTextBlock.Text = (string)((ComboBoxItem)seasonConboBox.SelectedItem).Content;
+            //seasonTextBlock.Text = (string)((ComboBoxItem)seasonConboBox.SelectedItem).Content;
+        }
+    }
+}
