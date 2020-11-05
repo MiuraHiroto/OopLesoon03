6bba3de95a6fd5f43376c733e051459d6a2ba545
MiuraHiroto <31021@ojs.ac.jp>
2020-11-05 10:40:23 +0900

    CC 途中経過

--- SendMailApp/MainWindow.xaml.cs
+++ SendMailApp/MainWindow.xaml.cs
@@ -50,7 +50,12 @@ namespace SendMailApp
             try
             {
                 MailMessage msg = new MailMessage("ojsinfosys01@gmail.com", tbTo.Text);
-                msg.CC.Add(new MailAddress(tbCc.Text));
+                var cc = tbCc.Text.Split(',');
+                foreach (var item in cc)
+                {
+                    msg.CC.Add(new MailAddress(item));
+                }
+                
                 msg.Subject = tbTitle.Text; //件名
                 msg.Body = tbBody.Text; //本文
 
