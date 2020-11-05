554eb9b76a59a7fe41234264f41da09daea7c179
MiuraHiroto <31021@ojs.ac.jp>
2020-11-05 11:20:36 +0900

    設定クラス作成

---
+++ SendMailApp/Config.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendMailApp
+{
+    class Config
+    {
+        public string Smtp { get; set; }    //smtpサーバ
+        public string MailAddress { get; set; } //自メールアドレス(送信先)
+        public string PassWord { get; set; }    // パスワード
+        public int port { get; set; }    //ポート番号
+        public bool Ssl { get; set; }     //SSL設定
+
+        //初期設定用
+        public void DefaultSet()
+        {
+            Smtp = "smtp.gmail.com";
+            MailAddress = "ojsinfosys01@gmail.com";
+            PassWord = "ojsInfosys2020";
+            port = 587;
+            Ssl = true;
+
+        }
+        
+    }
+}
